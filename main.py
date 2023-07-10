b319afc93e8304e01e9cf9a5fcab681be46905fd
mjcortejo <mark_jethro27@yahoo.com>
2023-07-10 18:48:51 +0800

    add 1 more line to form an intersection

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ import tkinter as tk
 import random
 import networkx as nx
 
+"""
+Create network graph representation
+"""
+
 G = nx.Graph()
 
 intersection_nodes = {
     1: (100, 100),
-    2: (100, 200)
+    2: (100, 200),
+    3: (200, 200)
 }
 
 edges = [
-    (1, 2)
+    (1, 2),
+    (2, 3)
 ]
 
 for index, pos in intersection_nodes.items():
@@ -21,21 +27,23 @@ G.add_edges_from(edges)
 
 print(G)
 
+"""
+Now drawing the road network using the graph
+"""
+
 env = simpy.Environment()
 root = tk.Tk()
 canvas = tk.Canvas(root, width=800, height=600)
 canvas.pack()
 
-"""
-Now drawing the road network using the graph
-"""
-car_radius = 3
+
+intersection_radius = 3
 
 def draw_intersection(x, y):
-    x0 = x - car_radius
-    y0 = y - car_radius
-    x1 = x + car_radius
-    y1 = y + car_radius
+    x0 = x - intersection_radius
+    y0 = y - intersection_radius
+    x1 = x + intersection_radius
+    y1 = y + intersection_radius
     canvas.create_oval(x0, y0, x1, y1, fill="blue")
 
 def draw_line_from_edge(a, b):
