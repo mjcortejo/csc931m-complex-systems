a1f502b4b6e85ca90a33bfc8b1a26b9aa41dd710
mjcortejo <mark_jethro27@yahoo.com>
2023-07-12 14:40:29 +0800

    refactor code to use car class and animate movement

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+import math
 import simpy
 import tkinter as tk
 import random
 import networkx as nx
 import threading
+import numpy as np
 
 """
 Create network graph representation
 """
-
 G = nx.Graph()
 lane_offset = 10
 
@@ -71,19 +72,103 @@ def place_car(x, y, car_radius=3):
     car = canvas.create_oval(x0, y0, x1, y1, fill="yellow")
     return car
 
+"""
+Draw the nodes as intersection junctions
+"""
 for index, pos in intersection_nodes.items():
     draw_intersection(*pos)
 
+"""
+Draw a line using the edge information
+"""
 for edge in edges:
     draw_line_from_edge(*edge)
 
-# number_of_cars = 1
-car1 = place_car(100, 100)
+
+"""
+Car Class
+"""
+class Car:
+    def __init__(self, index):
+        self.index = index
+        self.pos_x = None
+        self.pos_y = None
+        self.org_x = None
+        self.org_y = None
+        self.des_x = None
+        self.des_y = None
+        
+        self.speed = 1
+        self.car = None
+        self.car_radius = 3
+    
+    def place_car(self, x, y):
+        x0 = x - self.car_radius
+        y0 = y - self.car_radius
+        x1 = x + self.car_radius
+        y1 = y + self.car_radius
+
+        self.pos_x = x
+        self.pos_y = y
+        self.car = canvas.create_oval(x0, y0, x1, y1, fill="yellow")
+            # return car
+    
+    def move_to(self, x, y):
+        x0 = x - self.car_radius
+        y0 = y - self.car_radius
+        x1 = x + self.car_radius
+        y1 = y + self.car_radius
+        
+        # self.pos_x = x
+        # self.pos_y = y
+        canvas.coords(self.car, x0, y0, x1, y1)
+
+    def set_origin(self, origin_x, origin_y):
+        self.org_x = origin_x
+        self.org_y = origin_y
+
+    def set_destination(self, destination_x, destination_y):
+        self.des_x = destination_x
+        self.des_y = destination_y
+    
+
+"""
+Draw cars in the grid, and assign their origin and destination
+"""
+number_of_cars = 1
+cars = []
+for index in range(number_of_cars):
+    car = Car(index)
+    edge_choice = random.choice(edges)
+    # for edge in edges():
+    p1 = intersection_nodes[edge[0]]
+    p2 = intersection_nodes[edge[1]]
+
+    #place at middle part of those edges for now
+    midpoint_x = (p1[0] + p2[0]) / 2
+    midpoint_y = (p1[1] + p2[1]) / 2
+
+    car.set_origin(p1[0], p1[1]) #p1 is x and y respectively
+    car.place_car(midpoint_x, midpoint_y)
+    car.set_destination(p2[0], p2[1]) #p2 is x and y respectivelyu
+    # car = place_car(midpoint_x, midpoint_y)
+    cars.append(car)
+
 
 def task(env):
     while True:
-        canvas.move(car1, 0, 1)
-        print(canvas.coords(car1))
+        for each_car in cars:
+            dx = each_car.des_x - each_car.pos_x
+            dy = each_car.des_y - each_car.pos_y
+            distance = math.sqrt(dx ** 2 + dy ** 2)
+            if distance > 1:
+                step = min(each_car.speed, distance)
+                each_car.pos_x += (dx / distance) * step
+                each_car.pos_y += (dy / distance) * step
+                each_car.move_to(each_car.pos_x, each_car.pos_y)
+
+        # canvas.move(car1, 0, 1)
+        # print(canvas.coords(car1))
         yield env.timeout(1)
 
 
