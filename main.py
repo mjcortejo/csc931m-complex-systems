b89f097045e7583eef46a5b6c674cc586b3c37b9
mjcortejo <mark_jethro27@yahoo.com>
2023-07-20 12:13:45 +0800

    Add comments for all functions

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ class TrafficManager():
 
     def change_light_state(self, intersection_node, neighboring_node, color_state=None, timer=None):
         #change color state
+        # Set color state to red if color is red or red
         if color_state is None:
             if self.intersection_states[intersection_node][neighboring_node]["color"] == "red":
                 color_state = "green"
@@ -50,16 +51,34 @@ class TrafficManager():
         self.intersection_states[intersection_node][neighboring_node]["timer"] = self.default_intersection_time if timer is None else timer
 
     def get_intersection_light_state(self, intersection_node, neighboring_node):
-        # print(f"intersection_node: {intersection_node} origin_node: {neighboring_node}")
+        """
+         Get the light state of a neighboring node. This is used to determine whether or not a node is intersecting another node.
+         
+         @param intersection_node - The node that is intersecting.
+         @param neighboring_node - The node that is a neighboring node.
+         
+         @return The light state of the neighboring node in the color
+        """
         return self.intersection_states[intersection_node][neighboring_node]["color"]
     
     def destination_has_intersection(self, intersection_node):
+        """
+         Checks if the destination node has an intersection. This is used to determine if there is a point in the destination to be intersected with the source
+         
+         @param intersection_node - The node that we are interested in
+         
+         @return True if the destination node has an intersection False if
+        """
+        # Returns true if the intersection node is in the intersection states
         if intersection_node in self.intersection_states:
             return True
         else:
             return False
 
     def __build_network__(self):
+        """
+         Builds the network
+        """
         self.intersection_nodes = {
             #ENTRY NODES
             'E1': (200, 50), #just above Node 2
@@ -86,13 +105,16 @@ class TrafficManager():
 
         self.edges = {i: {'cars_occupied': [], 'has_accident': False, 'road_speed': 50, 'one_way': False} for i in self.edge_list}
 
+        # Add a node to the graph.
         for index, pos in self.intersection_nodes.items():
+            # Add index to the list of entries in the entry_nodes list.
             if type(index) == str and "E" in index:
                 self.entry_nodes.append(index)
             self.G.add_node(index, pos=pos)
 
+        # Add edges to the graph.
         for edges in self.edge_list:
-            # if type(index) == str and "E" in [edges[0], edges[1]]:
+            # Add edges to the entry edges list
             if any(isinstance(edge, str) for edge in edges) and any("E" in edge for edge in edges):
                 #for now we will assume that the entry edge is at the first element
                 self.entry_edges.append(edges)
@@ -100,12 +122,14 @@ class TrafficManager():
             self.G.add_edge(edges[0], edges[1])
             self.G.add_edge(edges[1], edges[0])
 
-        #loop all nodes and check which nodes have more than 2 edges, and apply intersection states for each edge
+        # Loop all nodes and check which nodes have more than 2 edges, and apply intersection states for each edge
         for n in self.G:
             # print(f"G Degree of {n}: {self.G.degree[n]}")
+            # Apply intersection light states between nodes
             if self.G.degree[n] > 2: #check if node has more than 2 neighbors then apply intersection light states
                 neighbor_nodes = list(self.G.neighbors(n))
                 self.intersection_states[n] = {}
+                # This function is used to generate a dictionary of light states between nodes and neighbors
                 for index, neighbor in enumerate(neighbor_nodes): #needed to enumerate so I can use module to alternate values
                     color_state = "green"
                     if index % 2 == 0: #alternate light states between nodes
@@ -115,30 +139,13 @@ class TrafficManager():
                         "timer": self.default_intersection_time
                     }
 
-        """
-        Draw the nodes as intersection junctions
-        """
+        # Draw the intersection of all nodes in the intersection_nodes.
         for index, pos in self.intersection_nodes.items():
             self.__draw_intersection__(*pos, index=index)
 
-        """
-        Draw a line using the edge information
-        """
+        # Draw all lines from the edges of the graph.
         for edge in self.edges:
             self.__draw_line_from_edge__(*edge)
-            
-    # def add_car_to_edge(self, car_index, origin, destination):
-    #     #check the orientation of origin,destination in the tuple
-    #     orientation1 = (origin, destination)
-    #     orientation2 = (destination, origin)
-    #     if orientation1 in self.edges.keys() or orientation2 in self.edges.keys():
-    #         self.edges[orientation1]['cars_occupied'].append(car_index)
-
-    # def remove_car_from_edge(self, car_index, origin, destination):
-    #     orientation1 = (origin, destination)
-    #     orientation2 = (destination, origin)
-    #     if orientation1 in self.edges.keys() or orientation2 in self.edges.keys():
-    #         self.edges[orientation1]['cars_occupied'].remove(car_index)
 
     def manage_car_from_edge(self, car_index: int, origin: int, destination: int, how: str):
         orientation = None
@@ -175,8 +182,15 @@ class TrafficManager():
 
     def __draw_intersection__(self, x, y, index=None, offset=5, color="blue"):
         """
-        Now drawing the road network using the graph
+         Draw the road network using the graph. This is the function that is called by __draw_network
+         
+         @param x - X coordinate of the top left corner of the graph
+         @param y - Y coordinate of the top left corner of the graph
+         @param index - Index of the node to draw. If None the node is drawn at the edge
+         @param offset - Offset to draw the node at
+         @param color - Color of the node
         """
+
         x0 = x - self.intersection_radius
         y0 = y - self.intersection_radius
         x1 = x + self.intersection_radius
@@ -259,6 +273,13 @@ class Car:
         tm.manage_car_from_edge(self.index, self.origin_node, self.next_destination_node, how="add")
 
     def spawn(self, origin, next_immediate_destination, final_destination):
+        """
+         Spawn car at the origin node (usually an entry node). This is used to generate an edge from origin to next_immediate_destination
+         
+         @param origin - origin of car to spawn
+         @param next_immediate_destination - next destination of car to spawn
+         @param final_destination - final destination of car to spawn ( can be same
+        """
         p1 = tm.intersection_nodes[origin]
         # p2 = tm.intersection_nodes[next_immediate_destination]
 
@@ -272,7 +293,9 @@ class Car:
         self.is_spawned = True
 
     def travel(self):
-        #get the paths
+        """
+         Move the car to the next destination based on the speed and distance. This is called every frame
+        """
         des_x, des_y = tm.intersection_nodes[self.next_destination_node]
 
         dx = des_x - self.pos_x #use euclidean distance to judge the movement of the car even in an angle
@@ -280,18 +303,22 @@ class Car:
         distance = math.sqrt(dx ** 2 + dy ** 2)
 
         def __move():
+            """
+             Move the agent to the next position based on the speed and distance. This is called by the move ()
+            """
             step = min(self.speed, distance)
             self.pos_x += (dx / distance) * step
             self.pos_y += (dy / distance) * step
             self._move_to(self.pos_x, self.pos_y)
 
         # Get the cars that are in the same edge as the current car
-
+        # This method is called when the distance is below the light observation distance threshold.
         if distance > self.light_observation_distance:
             # cars_in_same_edge = tm.get_cars_in_edge(self.origin_node, self.next_destination_node)
             __move()
 
         elif distance > 0:
+            # Move the destination to the next destination node if the intersection is green
             if tm.destination_has_intersection(self.next_destination_node):
                 if tm.get_intersection_light_state(self.next_destination_node, self.origin_node) == "red":
                     #do not move if intersection is red
